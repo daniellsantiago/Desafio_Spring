b1a871d3849c266ba92eafd4ae365b7a7d967bb5
Daniel Santiago <daniel.psantiago@mercadolivre.com>
2022-10-05 10:58:05 -0300

    add initial project structure

---
+++ src/main/java/com/grupo2/desafiospring/model/Product.java
@@ -0,0 +1,21 @@
+package com.grupo2.desafiospring.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private Long productId;
+    private String name;
+    private String category;
+    private String brand;
+    private BigDecimal price;
+    private Integer quantity;
+    private Boolean freeShipping;
+    private String prestige;
+}
---
+++ src/main/java/com/grupo2/desafiospring/service/ProductServiceImpl.java
@@ -0,0 +1,14 @@
+package com.grupo2.desafiospring.service;
+
+import com.grupo2.desafiospring.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+}
